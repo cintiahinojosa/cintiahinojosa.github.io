5c4c7f30ec233a5e3e238d4338e391180d64d800
cintiahinojosa <38702817+cintiahinojosa@users.noreply.github.com>
2023-03-01 14:39:05 -0600

    add Rmd and images folder

---
+++ load.R
@@ -0,0 +1,18 @@
+# 1-LOAD KEYS & FUNCTIONS ---------------------------
+# load(file = "/Users/cintiahinojosa/Box/Dissertation/data/dissertation-keys.RData")
+
+# 2-LOAD CLEANED DATASETS ---------------------------
+# load(file = "/Users/cintiahinojosa/Box/Dissertation/data/clean-s1.RData")
+# load(file = "/Users/cintiahinojosa/Box/Dissertation/data/clean-s2.RData")
+# load(file = "/Users/cintiahinojosa/Box/Dissertation/data/clean-s3.RData")
+
+# 3-CLEANED DATASETS USED FOR ANALYSES ---------------------------
+# load(file = "/Users/cintiahinojosa/Box/Dissertation-Proposal/Data/s1.RData")
+# load(file = "/Users/cintiahinojosa/Box/Dissertation-Proposal/Data/s2.RData")
+# load(file = "/Users/cintiahinojosa/Box/Dissertation-Proposal/Data/s3.RData")
+
+# 4-CLEANED DATA TO USE BEFORE ANALYSES & PLOTS ---------------------------
+# load(file = "/Users/cintiahinojosa/Box/Dissertation/data/dissertation-starter.RData") 
+
+# 5-CLEANED DATA SAVED AFTER ANALYSES & PLOTS ---------------------------
+# load(file = "/Users/cintiahinojosa/Box/Dissertation/data/dissertation-workspace.RData")
